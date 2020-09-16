2c07cef1b4130bbc3d79d961f57e4eb813f3ff93
MykolaYurochko <koljaa94@gmail.com>
2020-09-16 10:24:22 +0300

    generated deployables

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyDmm6kNC4YFmBzmfVO1daqXURVgUFIiuIg",
+    authDomain: "musik-lib.firebaseapp.com",
+    databaseURL: "https://musik-lib.firebaseio.com",
+    projectId: "musik-lib",
+    storageBucket: "musik-lib.appspot.com",
+    messagingSenderId: "871817499684",
+    appId: "1:871817499684:web:de2b2514cc7284cf84b4b1",
+    measurementId: "G-8M2HFYLDFV"
+  }
 };
